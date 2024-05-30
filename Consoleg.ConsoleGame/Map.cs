121d52fa869d0b3db054c083daae3b816f7e1c15
marcus pedersen <marcus@mpedersen.me>
2024-05-30 14:28:19 +0200

    Add project files.

---
+++ Consoleg.ConsoleGame/Map.cs
@@ -0,0 +1,10 @@
+﻿internal class Map
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Map(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
