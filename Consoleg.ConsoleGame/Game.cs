8281ac02c1085f00525480a668d400d7666382bf
marcus pedersen <marcus@mpedersen.me>
2024-06-03 14:30:00 +0200

    bug fixed printing symbol

--- Consoleg.ConsoleGame/Game.cs
+++ Consoleg.ConsoleGame/Game.cs
@@ -66,7 +66,7 @@ internal class Game
                 }
 
                 Console.ForegroundColor = drawable.Color;
-                Console.Write(cell.Symbol);
+                Console.Write(drawable.Symbol);
             }
             Console.WriteLine();
         }
