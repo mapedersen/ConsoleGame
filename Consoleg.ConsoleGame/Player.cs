31096b3c646b8ac23401cd8e81d8b7e919fc9b2f
marcus pedersen <marcus@mpedersen.me>
2024-06-03 13:57:05 +0200

    Creating creature class

--- Consoleg.ConsoleGame/Player.cs
+++ Consoleg.ConsoleGame/Player.cs
@@ -5,22 +5,3 @@
         Color = ConsoleColor.White;
     }
 }
-
-internal class Creature
-{
-    public Cell Cell { get; }
-    public string Symbol { get; }
-    public ConsoleColor Color { get; protected set; } = ConsoleColor.Green;
-    public Creature(Cell cell, string symbol)
-    {
-        Cell = cell ?? throw new ArgumentNullException(nameof(cell));
-        
-        if (string.IsNullOrEmpty(symbol))
-        {
-            throw new ArgumentException($"'{nameof(symbol)}' cannot be null or empty.", nameof(symbol));
-        }
-
-        Symbol = symbol;
-    }
-
-}
