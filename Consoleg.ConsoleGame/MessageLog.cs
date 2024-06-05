d730c53fee33595dbe3fc682abd85f8fbb118720
marcus pedersen <marcus@mpedersen.me>
2024-06-05 13:27:08 +0200

    Extending LimitedList with messageLog

---
+++ Consoleg.ConsoleGame/MessageLog.cs
@@ -0,0 +1,22 @@
+﻿using Consoleg.LimitedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleg.ConsoleGame
+{
+    internal class MessageLog<T> : LimitedList<T>
+    {
+        public MessageLog(int capacity) : base(capacity) {}
+
+        public override bool Add(T item)
+        {
+            ArgumentNullException.ThrowIfNull(item, nameof(item));
+
+            if (IsFull) _list.RemoveAt(0);
+            return base.Add(item);
+        }
+    }
+}
